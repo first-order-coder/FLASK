a930dfdda8be8636aa54f56fbd0b08b1194d8dab
first-order-coder <manethginura@gmail.com>
2025-07-23 23:01:27 +0300

    cmt_02

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,6 +1,26 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
+def home():
+    return "Hellow this is the home page"
+
 @app.route('/index')
 def index():
-    return "Hello, World! its the first deployment"
+#so instead of writing the html code like this without any autofill options we can use templates to seperate these files and import them later.
+    user = {'username': 'Isildur'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Hardest choices require the strongest wills!'
+        },
+        {
+            'author': {'username': 'Erika'},
+            'body': 'From a place that u wont see, comes a sound that u wont hear, just a flash of light!'
+        },
+        {
+            'author': {'username': 'Thanos'},
+            'body': 'You could not live with your own faliure, and where did that bring you? back to me.'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
