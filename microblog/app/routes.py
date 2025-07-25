7bd39a29868bbacd04be2a134bac61c0dc98f301
first-order-coder <manethginura@gmail.com>
2025-07-25 11:13:38 +0300

    cmt_03

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,5 +1,8 @@
 from app import app
 from flask import render_template
+from app.forms import LoginForm
+from flask import render_template, flash, redirect
+from flask import url_for
 
 @app.route('/')
 def home():
@@ -24,3 +27,14 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST']) #view function accepts GET and POST requests 
+def login():
+    form = LoginForm() #created instance of the form needed for login.html
+    if form.validate_on_submit(): #does all the form processing work
+        flash(f'Login requested for user {form.username.data}, remember_me={form.remember_me.data}') # flash stores the measage in the session and it was displayed using HTML in login.html 
+        #flashed messages appear only once after the flash() function is called and the are removed after the get_flashed_messages function
+        return redirect(url_for('index')) #if block is executed only when
+                                        #>> the form was submitted (method == post) and all fields pass validation
+    return render_template('login.html', title='Sign In', form=form)
+# if the form had erros or hasn't submitted or hasn't validated properly then the return render_template will just show the loging page again
