214c5669b72af720052acb47ab9dfcaf08c407a6
first-order-coder <manethginura@gmail.com>
2025-07-28 09:21:17 +0300

    cmt_05

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -1,14 +1,28 @@
+from datetime import datetime, timezone
 from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db
 
-class User(db.Model):
+class User(db.Model): #if db.Model isn't used then all the mappings have to be done manually
+    __tablename__ = 'db_User'
     id: so.Mapped[int] = so.mapped_column(primary_key=True) #these are python type hints
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True) #stores string upto 64 characters and indexed for fast lookup and is unique no duplicate usernames
+
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
+    posts: so.WriteOnlyMapped['Post'] = so.relationship(back_populates='author')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+class Post(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    body: so.Mapped[str] = so.mapped_column(sa.String(140))
+    timestamp: so.Mapped[datetime] = so.mapped_column(index=True, default=lambda: datetime.now(timezone.utc))
+    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id), index=True)
+    author: so.Mapped[User] = so.relationship(back_populates='posts')
+
+    def __repr__(self):
+          return '<Post {}>'.format(self.body)  
     
